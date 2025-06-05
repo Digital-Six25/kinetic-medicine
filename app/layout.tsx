0099f322db8da218095a9ec78cf00df79e8b4716
sarvesh-rumsan <sarvesh.karki@rumsan.net>
2025-06-05 10:19:09 +0545

    build correction

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Footer } from "@/components/Footer";
+import { Footer } from "../components/Footer";
 import { Navbar } from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
